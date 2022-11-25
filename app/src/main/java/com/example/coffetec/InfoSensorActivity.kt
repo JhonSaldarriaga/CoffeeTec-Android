af09ad8e8a987901d3905858a0b33f6e4ffc66df
Valentina <valeariasparra2001@gmail.com>
2022-11-25 00:58:13 -0500

    creates the desing and variables in each activity and fragment

---
+++ app/src/main/java/com/example/coffetec/InfoSensorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.coffetec
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InfoSensorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_info_sensor)
+    }
+}
