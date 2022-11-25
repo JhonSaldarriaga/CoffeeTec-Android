79433b4add57053a4d686d3bfa2d058c9f8c08f5
Valentina <valeariasparra2001@gmail.com>
2022-11-25 01:23:56 -0500

    develop the basic navigation using the buttons

--- app/src/main/java/com/example/coffetec/RegistrationSensorActivity.kt
+++ app/src/main/java/com/example/coffetec/RegistrationSensorActivity.kt
@@ -1,11 +1,32 @@
 package com.example.coffetec
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.coffetec.databinding.ActivityRegistrationSensorBinding
+import com.example.coffetec.databinding.FragmentSensorsDashboardBinding
 
 class RegistrationSensorActivity : AppCompatActivity() {
+
+
+    lateinit var binding: ActivityRegistrationSensorBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_registration_sensor)
+        binding = ActivityRegistrationSensorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cancellRegistrationSensorButton.setOnClickListener {
+            finish()
+        }
+
+        binding.registSensorButton.setOnClickListener {
+            val intent = Intent(this, InfoSensorActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.backAuxSensorHeader.setOnClickListener {
+            finish()
+        }
     }
 }
