2162a0cda276bf1086ebbcad399c92fba4954464
Valentina <valeariasparra2001@gmail.com>
2022-11-27 16:21:05 -0500

    works sensors and harvest

--- app/src/main/java/com/example/coffetec/HomeActivity.kt
+++ app/src/main/java/com/example/coffetec/HomeActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.coffetec.databinding.ActivityHomeBinding
 import com.example.coffetec.fragments.HarvestFragment
+import com.example.coffetec.sensors.SensorsDashboardFragment
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var harvestFragment: HarvestFragment
+    private lateinit var sensorsFragment: SensorsDashboardFragment
     private lateinit var newHarvest: NewHarvest
     private val binding : ActivityHomeBinding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
 
@@ -17,12 +19,13 @@ class HomeActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         harvestFragment = HarvestFragment.newInstance()
+        sensorsFragment = SensorsDashboardFragment.newInstance()
 
         //suscription
-
-
         binding.navigator.setOnItemSelectedListener { menuItem->
             when(menuItem.itemId){
+                R.id.harvestmenu -> {showFragment(harvestFragment)}
+                R.id.sensoresmenu -> {showFragment(sensorsFragment)}
                 R.id.harvestmenu -> {showFragment(harvestFragment)}
             }
             true
