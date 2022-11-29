922ba14bc8e00bc8f92d9486a414a2c733e74cc7
Jhon Saldarriaga <jhonsaldarriagalopez@gmail.com>
2022-11-29 02:24:52 -0500

    cafeto_row in fragment_trees has been updated

--- app/src/main/java/com/example/coffetec/recycler/TreesViewHolder.kt
+++ app/src/main/java/com/example/coffetec/recycler/TreesViewHolder.kt
@@ -3,6 +3,7 @@ package com.example.coffetec.recycler
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.coffetec.R
 import com.example.coffetec.model.Tree
@@ -12,6 +13,7 @@ class TreesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     lateinit var tree: Tree
     lateinit var listener: Listener
     /// UI Components
+    private val rowLayout : ConstraintLayout = itemView.findViewById(R.id.row_layout)
     private val showButton : ImageView = itemView.findViewById(R.id.show_cafeto_row)
     private val showImageButton : ImageView = itemView.findViewById(R.id.show_image_cafeto_row)
     val name : TextView = itemView.findViewById(R.id.name_cafeto_row)
@@ -20,6 +22,9 @@ class TreesViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
 
     init {
         showButton.setOnClickListener {
+            listener.onClickShowTree(tree)
+        }
+        rowLayout.setOnClickListener {
             listener.onClickShowTree(tree)
         }
         showImageButton.setOnClickListener {
